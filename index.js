d69742603f73c22965ccd75a25f58f87342849d8
Syed Usama Ahmad <syedusama12345@gmail.com>
2020-12-23 15:28:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://nodejsapidownloader.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
